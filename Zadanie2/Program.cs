81f10b034018fb4959bc048c4d5133560ccb4c88
Daniel <Daniel@DESKTOP-7IMDU22>
2021-03-15 17:44:57 +0100

    Console configuration

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -7,10 +7,46 @@ namespace Zadanie2
         static void Main(string[] args)
         {
             Console.WriteLine("Enter array capacity:");
+
+            TableClass array = new TableClass();
+            array.setTabCapacity(askX(), askY());
+
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.WriteLine("Choose:");
+                Console.WriteLine("1.Read");
+                Console.WriteLine("2.Write");
+                int choice = Int32.Parse(Console.ReadLine());
+
+                if (choice == 1 | choice == 2)
+                {
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("Choose 1 or 2");
+                    continue;
+                }
+
+                
+            }
+
+
+
+        }
+
+        private static int askX()
+        {
             Console.Write("X:");
-            int x = Int32.Parse(Console.ReadLine());
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        private static int askY()
+        {
             Console.Write("Y:");
-            int y = Int32.Parse(Console.ReadLine());
+            return Int32.Parse(Console.ReadLine());
         }
     }
 }
